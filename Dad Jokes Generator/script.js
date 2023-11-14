6e3c440156c4b8ebea89ed56ec71d005c54f267b
rhyz47 <vas_x9@hotmail.com>
2023-11-14 18:21:12 +0200

    new things

---
+++ Dad Jokes Generator/script.js
@@ -0,0 +1,21 @@
+const jokeElement = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+function generateJoke() {
+    const config = {
+        headers: {
+        'Accept' : 'application/json'
+    }}
+    fetch('https://icanhazdadjoke.com', config)
+    .then(res => res.json())
+    .then(data => {
+            jokeElement.innerHTML = data.joke
+        })
+
+}
+
+    
